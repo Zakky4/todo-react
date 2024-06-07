03728eafcb4ccd025f843d607af9d04400edd297
zakky <sadaoza@gmail.com>
2024-06-08 07:43:42 +0900

    CSS setting

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -3,28 +3,28 @@ import "./style.css";
 export const Todo = () => {
   return (
     <>
-      <div class="input-area">
+      <div className="input-area">
         <input id="add-text" placeholder="TODOを入力" />
         <button id="add-button">追加</button>
       </div>
-      <div class="incomplete-area">
-        <p class="title">未完了のTODO</p>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
         <ul id="incomplete-lst">
           <li>
-            <div class="list-row">
-              <p class="todo-item">TODOです</p>
+            <div className="list-row">
+              <p className="todo-item">TODOです</p>
               <button>完了</button>
               <button>削除</button>
             </div>
           </li>
         </ul>
       </div>
-      <div class="complete-area">
-        <p class="title">完了したTODO</p>
+      <div className="complete-area">
+        <p className="title">完了したTODO</p>
         <ul id="complete-lst">
           <li>
-            <div class="list-row">
-              <p class="todo-item">TODOです</p>
+            <div className="list-row">
+              <p className="todo-item">TODOです</p>
               <button>戻す</button>
             </div>
           </li>
