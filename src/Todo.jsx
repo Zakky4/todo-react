38b651dc78ad0dc0c90b20473383a6b7cb170a7a
zakky <sadaoza@gmail.com>
2024-06-08 10:14:19 +0900

    Add Task

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -2,19 +2,40 @@ import { useState } from "react";
 import "./style.css";
 
 export const Todo = () => {
+  // 入力したタスク
+  const [todoText, setTodoText] = useState("");
+  // 未完了のTODO
   const [incompleteTodos, setIncompleteTodos] = useState([
     "TODOです1",
     "TODOです2",
   ]);
+  // 完了したTODO
   const [completeTodos, setCompleteTodos] = useState([
     "TODOでした1",
     "TODOでした2",
   ]);
+
+  // 入力したTODOを検知
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+  
+  // TODOを追加
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+
   return (
     <>
       <div className="input-area">
-        <input id="add-text" placeholder="TODOを入力" />
-        <button id="add-button">追加</button>
+        <input
+          id="add-text"
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button id="add-button" onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
